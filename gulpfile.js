384387b2048c540905a27049bd4f4f7f4ef3edc7
Andrew <andriy.kozovyi@gmail.com>
2024-09-11 21:00:46 +0300

    updated js script

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,72 @@
-// Підключаємо gulp
-var gulp = require("gulp");
+import gulp from 'gulp';
+//import scss from 'gulp-scss';
+import cssnano from 'gulp-cssnano';
+import autoprefixer from 'gulp-autoprefixer';
+import imagemin from 'gulp-imagemin';
+import concat from 'gulp-concat';
+import uglify from 'gulp-uglify';
+import rename from 'gulp-rename';
+import browserSync from 'browser-sync'; // Add BrowserSync
+//import {rimraf} from 'rimraf'; // Add Rimraf
 
-// Створюємо тестовий таск
-gulp.task('testTask', function (done) {
-    console.log('This is a test task!');
-    done(); // Завершуємо таск викликом функції done
+//import * as sassCompiler from 'scss';
+//const compileSass = scss(sassCompiler);
+
+//Use Rimraf to clean dist directory
+//gulp.task('clean', function (cb) {
+//    rimraf('dist', { glob: false }).then(() => cb()).catch(cb);});
+
+
+gulp.task('html', function () {
+    return gulp.src('app/*.html')
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSync.stream())
 });
 
-// Запуск тасків за замовчуванням
-gulp.task('default', gulp.series('testTask'));
+gulp.task('scss', function () {
+    return gulp.src('app/scss/*.scss')
+        .pipe(concat('styles.scss'))
+        //.pipe(compileSass().on('error', compileSass.logError))
+        .pipe(autoprefixer({
+            //overrideBrowserslist: ['last 2 versions'],
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cssnano())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src('app/js/*.js')
+        .pipe(concat('scripts.js'))
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist1/js'))
+        .pipe(browserSync.stream())
+});
+
+gulp.task('imgs', function () {
+    return gulp.src('app/img/*.+(jpg|jpeg|png|gif)')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            interlaced: true
+        }))
+        .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('watch', function () {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        },
+    })
+    gulp.watch('app/*.html', gulp.series('html')).on('change',browserSync.reload);
+    gulp.watch('app/js/*.js', gulp.series('scripts'));
+    gulp.watch('app/scss/*.scss', gulp.series('scss'));
+    gulp.watch('app/img/*.+(jpg|jpeg|png|gif)', gulp.series('imgs'));
+});
+
+//+clean -> + ) in the end
+gulp.task('default',/*gulp.series('clean', */gulp.series( 'html', 'scss', 'scripts', 'imgs' , 'watch'));
